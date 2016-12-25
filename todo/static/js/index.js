42e53114751da6b7c9d08a77ece6cbda67068dd3
karlin13 <skfk0135@gmail.com>
2016-12-25 13:04:14 +0900

    defined new function that does select all

--- todo/static/js/index.js
+++ todo/static/js/index.js
@@ -1,24 +1,10 @@
 
-//좀 더 명료한 함수이름으로 바꾸기
-// 모든 체크박스를 master(?체크박스임)상태와 같게 만든다
-function syncAllCheckbox(master){
-    var masterChecked = master.checked;
-
-    var todoCheckboxes = document.getElementsByName('todo_checkbox');
-
-    for(var idx=0;idx<todoCheckboxes.length;idx++){
-
-        if(todoCheckboxes[idx].checked != masterChecked) {
-            todoCheckboxes[idx].click();
-        }
-
-    }
-}
-
-function sendPost(cur_url, req_url, todo_id){
+function sendPost(cur_url, req_url, queryString){
     var xhr = new XMLHttpRequest();
 
     var url = req_url;
+
+    console.log(url);
 
     xhr.open("POST", url, true);
 
@@ -29,7 +15,8 @@ function sendPost(cur_url, req_url, todo_id){
     };
 
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-    xhr.send("todo_id="+todo_id);
+    console.log(queryString);
+    xhr.send(queryString);
 }
 
 function remove_todo_visible(todo_area){
